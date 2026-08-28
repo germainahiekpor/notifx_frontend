fa3a085d9ede348f8c7317ec6ef29efe71faeb48
germainahiekpor <jermainahiekpor@gmail.com>
2026-08-28 11:17:18 +0000

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const BASE = 'http://localhost:8080';
-export const WS_BASE = 'ws://localhost:8080';
+export const BASE = "http://102.68.83.23:8080";
+export const WS_BASE = "ws://102.68.83.23:8080";
